144a0619fbc572bb6fecf053cb240dd75ecb78c6
Abdul Wahab <awanabdulwahab7@gmail.com>
2021-08-21 16:53:13 +0500

    Making the UI Look More Professional

---
+++ WebBrowser/DataAccess.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using Windows.Storage;
+using Windows.Storage.Streams;
+
+namespace WebBrowser
+{
+    public class DataAccess
+    {
+        /// <summary>
+        /// Create a File named settings.xml to store the settings for a user
+        /// </summary>
+        public async void CreateSettingsFile()
+        {
+            try
+            {
+                var storageFile = await ApplicationData.Current.LocalFolder.CreateFileAsync("settings.xml");
+
+                using (IRandomAccessStream writeStream = await storageFile.OpenAsync(FileAccessMode.ReadWrite))
+                {
+                    Stream s = writeStream.AsStreamForWrite();
+                    XmlWriterSettings settings = new XmlWriterSettings();
+                    settings.Async = true;
+                    settings.Indent = true;
+                    using (XmlWriter writer = XmlWriter.Create(s, settings))
+                    {
+                        writer.WriteStartDocument();
+                        writer.WriteStartElement("settings");
+                        writer.WriteStartElement("history");
+                        writer.WriteEndElement();
+                        writer.WriteStartElement("bookmarks");
+                        writer.WriteEndElement();
+                        writer.WriteStartElement("searchEngine");
+                        writer.WriteStartElement("google");
+                        writer.WriteAttributeString("prefix", "https://www.google.com/search?q=");
+                        writer.WriteEndElement();
+                        writer.WriteEndElement();
+                        writer.WriteEndElement();
+                        writer.WriteEndDocument();
+                        writer.Flush();
+                        await writer.FlushAsync();
+                    }
+                }
+
+                await Windows.System.Launcher.LaunchFileAsync(storageFile);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ WebBrowser/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace WebBrowser
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            //DataAccess data = new DataAccess();
+            //data.CreateSettingsFile();
+        }
+
+        private void backBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (webBrowser.CanGoBack)
+            {
+                webBrowser.GoBack(); 
+            }
+        } 
+
+        private void forwardBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (webBrowser.CanGoForward)
+            {
+                webBrowser.GoForward(); 
+            }
+        }
+
+        private void txt_searchBar_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == Windows.System.VirtualKey.Enter)
+            {
+                Search();
+            }
+        }
+
+        //Custom Function
+
+        private void Search()
+        {
+            webBrowser.Source = new Uri("https://www.google.com/search?q=" + txt_searchBar.Text);
+        }
+    }
+}
