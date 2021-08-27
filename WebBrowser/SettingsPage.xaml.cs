369db7f596ec76408d4ac934579238004f458075
Abdul Wahab <awanabdulwahab7@gmail.com>
2021-08-27 17:19:01 +0500

    Added Settings Tabs and their Functionality

--- WebBrowser/SettingsPage.xaml.cs
+++ WebBrowser/SettingsPage.xaml.cs
@@ -26,5 +26,43 @@ namespace WebBrowser
         {
             this.InitializeComponent();
         }
+
+        private void SettingsView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            var selectedItem = (NavigationViewItem)args.SelectedItem;
+            string tag = ((string)selectedItem.Tag);
+            if (!args.IsSettingsSelected)
+            {
+                if (tag == "accountSetMenu")
+                {
+                    ContentFrame.Navigate(typeof(AccountSetMenu),null, args.RecommendedNavigationTransitionInfo);
+                }
+
+                else if (tag == "bookmarkSetMenu")
+                {
+                    ContentFrame.Navigate(typeof(BookmarkSetMenu), null, args.RecommendedNavigationTransitionInfo);
+                }
+
+                else if (tag == "historySetMenu")
+                {
+                    ContentFrame.Navigate(typeof(HistorySetMenu), null, args.RecommendedNavigationTransitionInfo);
+                }
+                else if (tag == "searchSetMenu")
+                {
+                    ContentFrame.Navigate(typeof(SearchSetMenu), null, args.RecommendedNavigationTransitionInfo);
+                }
+            }
+        }
+
+        private void SettingsView_BackRequested(NavigationView sender, NavigationViewBackRequestedEventArgs args)
+        {
+            this.Frame.Navigate(typeof(MainPage));
+
+        }
+
+        private void settingsPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            SettingsView.SelectedItem = SettingsView.MenuItems[0];
+        }
     }
 }
